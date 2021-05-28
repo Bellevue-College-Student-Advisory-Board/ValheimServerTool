9ae3026684669a1b9ef807af579c6bb489d4a1bc
liamstar97 <liamstar@gmail.com>
2021-05-28 00:22:03 -0600

    added method for reading AWS credentials from file

--- ServerMonitor/Program.cs
+++ ServerMonitor/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
 
 namespace ServerMonitor
 {
@@ -14,10 +16,28 @@ namespace ServerMonitor
         [STAThread]
         static void Main()
         {
+            AWSCredentials credentials = GetAWSCredentialsByName("valheim");
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static AWSCredentials GetAWSCredentialsByName(string profileName)
+        {
+            if (String.IsNullOrEmpty(profileName))
+            {
+                throw new ArgumentNullException("profileName cannot be null or empty");
+            }
+
+            SharedCredentialsFile credFile = new SharedCredentialsFile();
+            CredentialProfile profile = credFile.ListProfiles().Find(p => p.Name.Equals(profileName));
+            if (profile == null)
+            {
+                throw new Exception(String.Format("Profile named {0} not found", profileName));
+            }
+
+            return AWSCredentialsFactory.GetAWSCredentials(profile, new SharedCredentialsFile());
         }
     }
 }
